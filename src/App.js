29e0c1ce5a9e07bf5be54237c24417faaae8b554
Olga Tsybikova <tsy2701@gmail.com>
2022-04-25 22:06:42 +0200

    added gentlemen component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Gentleman from "./components/Gentlemen/Gentleman";
+import senyores from "./components/senyores";
+import "./index.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
+      </section>
+      <main className="main">
+        <ul className="gentlemen">
+          <Gentleman {...senyores[0]} />
+          <Gentleman {...senyores[1]} />
+          <Gentleman {...senyores[2]} />
+          <i className="icon gentleman__icon fas fa-check"></i>
+          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
